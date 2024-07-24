f89be421ec5b4de314d9580d2e68dde00f80915f
Ekaterina Nekhoroshkova <e_nekhoroshkova95@mail.ru>
2024-07-25 01:48:52 +0700

    final changes

--- main.py
+++ main.py
@@ -9,13 +9,13 @@
 # входной аргумент 73654108430135874305
 # выход функции **4305
 
-from src.masks import get_mask_account
-from src.masks import get_mask_card_number
+# from src.masks import get_mask_account
+# from src.masks import get_mask_card_number
 
 # user_card_number = input("Введите номер карты: ")
 # user_account_number = input("Введите номер счета: ")
 
-#Задание 2
+# Задание 2
 # Проверка работы функции, которая умеет обрабатывать информацию о картах и счетах
 # Пример для карты
 # Visa Platinum 7000792289606361  # входной аргумент
@@ -25,17 +25,17 @@ from src.masks import get_mask_card_number
 # Счет 73654108430135874305   входной аргумент
 # Счет **4305   выход функции
 
-from src.widget import mask_account_card
 from src.widget import get_data
+from src.widget import mask_account_card
 
 if __name__ == "__main__":
-    print(mask_account_card('Visa Platinum 7000792289606361'))
-    print(mask_account_card('Maestro 1596837868705199'))
-    print(mask_account_card('Счет 64686473678894779589'))
-    print(mask_account_card('MasterCard 7158300734726758'))
-    print(mask_account_card('Счет 35383033474447895560'))
-    print(mask_account_card('Visa Classic 6831982476737658'))
-    print(mask_account_card('Visa Platinum 8990922113665229'))
-    print(mask_account_card('Visa Gold 5999414228426353'))
-    print(mask_account_card('Счет 73654108430135874305'))
+    print(mask_account_card("Visa Platinum 7000792289606361"))
+    print(mask_account_card("Maestro 1596837868705199"))
+    print(mask_account_card("Счет 64686473678894779589"))
+    print(mask_account_card("MasterCard 7158300734726758"))
+    print(mask_account_card("Счет 35383033474447895560"))
+    print(mask_account_card("Visa Classic 6831982476737658"))
+    print(mask_account_card("Visa Platinum 8990922113665229"))
+    print(mask_account_card("Visa Gold 5999414228426353"))
+    print(mask_account_card("Счет 73654108430135874305"))
     print(get_data("2024-03-11T02:26:18.671407"))
