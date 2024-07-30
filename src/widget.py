4e96ce4e1dae352d500d8e7ef2dc3ede13985f3c
Ekaterina Nekhoroshkova <e_nekhoroshkova95@mail.ru>
2024-07-31 01:00:14 +0700

    add testing widget.py

--- src/widget.py
+++ src/widget.py
@@ -9,20 +9,29 @@ def mask_account_card(customer_details: str) -> str:
     from src.masks import get_mask_account
     from src.masks import get_mask_card_number
 
-    words_list = customer_details.split()
-    for word in words_list:
-        if word in ["Maestro", "Visa", "MasterCard"]:
-            user_card_number = words_list[-1]
-            masked_card = get_mask_card_number(user_card_number)
-            words_list[-1] = masked_card
-            result_mask_card = " ".join(words_list)
-            return result_mask_card
-        if word == "Счет" or word == "Счёт":
-            user_account_number = words_list[-1]
-            masked_account = get_mask_account(user_account_number)
-            words_list[-1] = masked_account
-            result_mask_account = " ".join(words_list)
-            return result_mask_account
+    if customer_details == '':
+        return 'Введено некорректное значение'
+    else:
+        words_list = customer_details.split()
+        for word in words_list:
+            if word in ["Maestro", "Visa", "MasterCard"]:
+                if words_list[-1].isdigit() is False:
+                    raise ValueError('Неверный номер карты')
+                else:
+                    user_card_number = words_list[-1]
+                    masked_card = get_mask_card_number(user_card_number)
+                    words_list[-1] = masked_card
+                    result_mask_card = " ".join(words_list)
+                return result_mask_card
+            if word == "Счет" or word == "Счёт":
+                if words_list[-1].isdigit() is False:
+                    raise ValueError('Неверный номер счета')
+                else:
+                    user_account_number = words_list[-1]
+                    masked_account = get_mask_account(user_account_number)
+                    words_list[-1] = masked_account
+                    result_mask_account = " ".join(words_list)
+                return result_mask_account
 
 
 # С помощью срезов выделяю в новую строку только те символы, которые нужны для форматирования даты.
@@ -31,8 +40,17 @@ def mask_account_card(customer_details: str) -> str:
 # Методом join склеиваю значения из списка обратно в строку, но в нужном порядке и с разделением ".".
 def get_data(enter_data: str) -> str:
     """Функция форматирует дату"""
-    slise_of_enter_data = enter_data[:10]
-    list_data = slise_of_enter_data.split("-")
-    list_data_reverse = list_data[::-1]
-    correct_data = ".".join(list_data_reverse)
-    return correct_data
+    if enter_data == '':
+        return 'Введено некорректное значение'
+    else:
+        slise_of_enter_data = enter_data[:10]
+        if "-" not in slise_of_enter_data:
+            return 'Введен некорректный формат даты'
+        else:
+            list_data = slise_of_enter_data.split("-")
+            for data_ in list_data:
+                if data_.isdigit() is False:
+                    return 'Введен некорректный формат даты'
+            list_data_reverse = list_data[::-1]
+            correct_data = ".".join(list_data_reverse)
+        return correct_data
--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,23 @@ def card_numbers():
 @pytest.fixture
 def account_numbers():
     return ['64686473678894779589', '35383033474447895560', '73654108430135874305']
+
+
+@pytest.fixture
+def customer_details():
+    return [
+        'Maestro 1596837868705199',
+        'Счет 64686473678894779589',
+        'MasterCard 7158300734726758',
+        'Счет 35383033474447895560',
+        'Visa Classic 6831982476737658',
+        'Visa Platinum 8990922113665229',
+        'Visa Gold 5999414228426353',
+        'Счет 73654108430135874305']
+
+@pytest.fixture
+def enter_data():
+    return [
+        '2024-03-11T02:26:18.671407',
+        '2018-06-30T02:08:58.425572',
+        '2018-09-12T21:27:25.241689']
