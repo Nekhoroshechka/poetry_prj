774714def38cbd3035cfc44a6c517a6d37b40334
Ekaterina Nekhoroshkova <e_nekhoroshkova95@mail.ru>
2024-07-30 23:42:05 +0700

    add testing masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,14 @@
 def get_mask_card_number(card_number: str) -> str:
     """Возвращает маску карты при получении номера карты"""
-    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    if len(card_number) == 16 and card_number.isdigit():
+        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    else:
+        return 'Введено некорректное значение номера карты'
 
 
 def get_mask_account(account_number: str) -> str:
     """Возвращает маску номера счета, полученного на входе функции"""
-    return f"**{account_number[-4:]}"
+    if len(account_number) == 20 and account_number.isdigit():
+        return f"**{account_number[-4:]}"
+    else:
+        return 'Введено некорректное значение номера счета'
---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+
+@pytest.fixture
+def card_numbers():
+    return ['1596837868705199', '7158300734726758', '6831982476737658']
+
+
+@pytest.fixture
+def account_numbers():
+    return ['64686473678894779589', '35383033474447895560', '73654108430135874305']
